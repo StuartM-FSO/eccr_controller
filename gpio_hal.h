d29e0f357e3da250a8842a694f5959414102ecf1
StuartM-FSO <stuartm.rec@gmail.com>
2026-06-19 22:09:17 +0100

    Add display & gpio hals

---
+++ gpio_hal.h
@@ -0,0 +1,26 @@
+
+#ifndef GPIO_HAL_H
+#define GPIO_HAL_H
+
+#include <Arduino.h>
+#include <stdint.h>
+
+typedef enum{
+  GPIO_STATUS_OK,
+  GPIO_STATUS_UNINITIALISED
+} gpiostate_t;
+
+typedef enum
+{
+  SWITCH_OFF = 0,
+  SWITCH_ON,
+  SWITCH_FAILURE,
+  SWITCH_UNINITIALISED
+} switchstate_t;
+
+gpiostate_t gpio_init(void);
+switchstate_t gpio_calibration_switch_on(void);
+switchstate_t gpio_select_button_pressed(void);
+void gpio_led_flash_error_state(const uint8_t error);
+
+#endif
